6af53a427d8f1667d32cb666d707f085279d6597
trox115 <tofernandes67@gmail.com>
2019-11-25 23:39:59 +0000

    Getting API for gif background

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const img = document.querySelector('#container');
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=wzRauOka5VGGkO1WRrBhRFPHBsxpeQqf&s=rain', {mode: 'cors'})
+.then(function(response) {
+   return response.json();
+ })
+ .then(function(response) {
+   img.style.backgroundImage = "url("+response.data.images.original.url+")";
+ });
