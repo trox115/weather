a0dc9dc2615d7710edb7c31ef2e28f44203837dc
trox115 <tofernandes67@gmail.com>
2019-11-26 23:27:58 +0000

    Some linter corrections

--- src/backgroud.js
+++ src/backgroud.js
@@ -1,17 +1,17 @@
 function apiBackground(weather) {
-  const img = document.querySelector("#container");
+  const img = document.querySelector('#container');
   console.log(weather);
   fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=wzRauOka5VGGkO1WRrBhRFPHBsxpeQqf&s=" +
+    'https://api.giphy.com/v1/gifs/translate?api_key=wzRauOka5VGGkO1WRrBhRFPHBsxpeQqf&s=' +
       weather,
-    { mode: "cors" }
+    { mode: 'cors' }
   )
     .then(function(response) {
       return response.json();
     })
     .then(function(response) {
       img.style.backgroundImage =
-        "url(" + response.data.images.original.url + ")";
+        'url(' + response.data.images.original.url + ')';
     });
 }
 
