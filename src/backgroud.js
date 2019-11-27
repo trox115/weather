714e810c453e207b71c79f16e7734eb2db2f24a2
trox115 <tofernandes67@gmail.com>
2019-11-27 16:47:11 +0000

    New linter

--- src/backgroud.js
+++ src/backgroud.js
@@ -2,16 +2,12 @@ function apiBackground(weather) {
   const img = document.querySelector('#container');
   console.log(weather);
   fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=wzRauOka5VGGkO1WRrBhRFPHBsxpeQqf&s=' +
-      weather,
-    { mode: 'cors' }
+    `https://api.giphy.com/v1/gifs/translate?api_key=wzRauOka5VGGkO1WRrBhRFPHBsxpeQqf&s=${weather}`,
+    { mode: 'cors' },
   )
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      img.style.backgroundImage =
-        'url(' + response.data.images.original.url + ')';
+    .then((response) => response.json())
+    .then((response) => {
+      img.style.backgroundImage = `url(${response.data.images.original.url})`;
     });
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import { apiBackground } from './backgroud';
 import { weatherApi } from './Api';
-let city = 'Évora';
-weatherApi(city).then(data => {
+
+const city = 'Évora';
+weatherApi(city).then((data) => {
   const weather = data.weather[0].main;
   const temperature = data.main.temp;
   const wind = data.wind.speed;
   const cit = data.name;
   const win = document.querySelector('.wind p');
   const condition = document.querySelector('.condition p');
-
-  console.log(data);
-
   const temp = document.querySelector('.temperature p');
   temp.innerText = temperature;
   const city = document.querySelector('.temperature2 p');
   city.innerText = cit;
-  win.innerText = wind + 'mph';
+  win.innerText = `${wind}mph`;
   condition.innerText = weather;
   apiBackground(weather);
 });
