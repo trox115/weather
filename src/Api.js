780d1632c68f9e5a6bc5161c57deed362e24c7c1
trox115 <tofernandes67@gmail.com>
2019-11-27 15:56:30 +0000

    Linter

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 async function weatherApi(city) {
   const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f8428a08b2b52c02fb71710a6f873f22`;
-  const response = await fetch(baseUrl, { mode: 'cors' });
+  const response = await fetch(baseUrl, { mode: "cors" });
   const data = await response.json();
   return data;
 }
