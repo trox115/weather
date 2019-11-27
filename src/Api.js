4df6144a2250eb511a3502bd53a880fd0ce79f72
trox115 <tofernandes67@gmail.com>
2019-11-27 21:14:46 +0000

    Added Celsius or fahrneit call

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,12 @@
-async function weatherApi(city) {
-  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f8428a08b2b52c02fb71710a6f873f22`;
+async function weatherApi(city, unit) {
+  let baseUrl;
+  console.log('hello');
+  console.log(unit);
+  if (unit == 'Fahrneit') {
+    baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f8428a08b2b52c02fb71710a6f873f22&units=imperial`;
+  } else {
+    baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f8428a08b2b52c02fb71710a6f873f22&units=metric`;
+  }
   const response = await fetch(baseUrl, { mode: 'cors' });
   const data = await response.json();
   return data;
