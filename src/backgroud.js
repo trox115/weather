5ec22537d1416ff22cae6bdf91949119bd67a893
trox115 <tofernandes67@gmail.com>
2019-11-27 23:32:34 +0000

    Fixed last linter error

--- src/backgroud.js
+++ src/backgroud.js
@@ -4,7 +4,7 @@ function apiBackground(weather) {
     `https://api.giphy.com/v1/gifs/translate?api_key=wzRauOka5VGGkO1WRrBhRFPHBsxpeQqf&s=${weather}`,
     { mode: 'cors' },
   )
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((response) => {
       img.style.backgroundImage = `url(${response.data.images.original.url})`;
     });
